2a69e2c8700ba7a58b85c4803a9c5d882376ad3b
TheSaviourEking <saviour.eking@outlook.com>
2024-04-05 01:34:47 +0100

    chore: implement props exercises

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import Showcase from './Showcase';
+import BaseStats from './BaseStats'
 import './App.css';
 
 function App() {
+  const basestats = {
+    hp: 45,
+    attack: 49,
+    defense: 49,
+    spAttack: 65,
+    spDef: 65,
+    speed: 45
+  }
+
+  function handleClick() {
+    alert(`Special Stats\n\t
+    Special Attack: ${basestats.spAttack}\n\t
+    Special Defense: ${basestats.spDef}`
+    );
+  }
+
   return (
     <div className='main-wrapper background'>
       <Showcase />
+      <BaseStats clicker={handleClick} stats={basestats} />
     </div>
   );
 }
